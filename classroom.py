0bf81e9bec758206ce28b044f86070c440834af0
junebride <blazequ3en@gmail.com>
2022-07-10 19:38:54 +0800

    Modified to create channel in current category Modified to allow user to create temp channel with custom time

--- classroom.py
+++ classroom.py
@@ -1,6 +1,5 @@
 import asyncio
 import discord
-import os
 from discord.ext import commands
 
 
@@ -8,15 +7,24 @@ class Classroom(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def create(self, ctx, channel_name):
-        guild = ctx.guild
-        await guild.create_text_channel(channel_name)
-        await ctx.send(f"Channel {channel_name} has been created")
-        await asyncio.sleep(10)
-        await ctx.invoke(self.bot.get_command("delete"), channel_name=channel_name)
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Bot is ready!")
 
-    @commands.command()
+    @commands.group(name="classroom", invoke_without_command=True)
+    async def classroom(self, ctx):
+        await ctx.send("Calling classroom")
+
+    @classroom.command()
+    async def create(self, ctx, channel_name, seconds=10):
+        category= ctx.channel.category
+        
+        await ctx.guild.create_text_channel(channel_name, category=category)
+        await ctx.send(f"Channel {channel_name} has been created")
+        await asyncio.sleep(seconds)
+        await ctx.invoke(self.classroom.get_command("delete"), channel_name=channel_name)
+
+    @classroom.command()
     async def delete(self, ctx, channel_name):
         channel = discord.utils.get(ctx.guild.channels, name=channel_name)
         await channel.delete()
